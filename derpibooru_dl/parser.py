a71bd3730d554797ae6bf59dae79d18d718065d0
mfg637 <mfg636@gmail.com>
2018-07-19 23:45:47 +0300

    parser bugfix

--- derpibooru_dl/parser.py
+++ derpibooru_dl/parser.py
@@ -16,7 +16,7 @@ def parseJSON(id:str):
 	return json.loads(str(rawdata, 'utf-8'))
 
 def download(outdir, data):
-	if 'file_name' in data:
+	if 'file_name' in data and data['file_name'] is not None:
 		filename=os.path.join(outdir, "{} {}.{}".format(data["id"],
 			re.sub('[/\[\]:;|=*".?]', '', os.path.splitext(data["file_name"])[0]),
 			data["original_format"]))
