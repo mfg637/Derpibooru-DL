3aeb54b69236304ef95cee2dd1559653039983d8
mfg637 <mfg636@gmail.com>
2018-10-30 22:09:04 +0200

    browser: unlook for load png with large metadata

--- browser/__init__.py
+++ browser/__init__.py
@@ -1,1 +1,7 @@
 from . import net, images, gui
+
+def pil_unlook_load_truncated_images():
+    import PIL.ImageFile
+    PIL.ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+pil_unlook_load_truncated_images()
