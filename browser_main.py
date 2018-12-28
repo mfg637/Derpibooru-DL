787800319e930a9aa7c28e7ed8cd1f4c20d1b1bb
mfg637 <mfg636@gmail.com>
2018-12-28 23:30:51 +0200

    change statistic message

--- browser_main.py
+++ browser_main.py
@@ -2,5 +2,10 @@
 # -*- coding: utf-8 -*- 
 
 import browser
+import config
 
 browser.gui.GUI()
+
+if config.enable_images_optimisations:
+    import derpibooru_dl.imgOptimizer
+    derpibooru_dl.imgOptimizer.printStats()
--- derpibooru_dl/imgOptimizer.py
+++ derpibooru_dl/imgOptimizer.py
@@ -194,9 +194,14 @@ def transcode(source, path, filename, data):
 
 def printStats():
 	if items:
-		print(('total save: {} MByte ({}%) average quality={} of {} pictures').format(
+		print(('total save: {} MBytes ({}%) from {} total MBytes \n'
+				'final size = {} MByte\n'
+				'average quality={} of {} pictures'
+				).format(
 			round((sumsize-sumos)/1024/1024, 2),
 			round((1-sumos/sumsize)*100,2),
+			round(sumsize/1024/1024, 2),
+			round(sumos/1024/1024, 2),
 			round(avq/items, 1),
 			items
 		))
