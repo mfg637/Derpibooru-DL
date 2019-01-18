27119b7d7c59206c48359c36ada4f363f7a3b874
mfg637 <mfg636@gmail.com>
2019-01-18 16:08:04 +0200

    remove unused code

--- browser/net.py
+++ browser/net.py
@@ -69,15 +69,3 @@ def parse_json(page_name, **kwargs):
     raw_data = connection.read()
     connection.close()
     return json.loads(str(raw_data, "utf-8"))
-
-
-def get_page(search=None, **kwargs):
-    if search is not None:
-        page_data = parse_json("search.json", q=search, page=page, **kwargs)
-    else:
-        page_data = parse_json("images.json", page=page, **kwargs)
-    if "images" in page_data:
-        return page_data["images"]
-    elif "search" in page_data:
-        return page_data["search"]
-
