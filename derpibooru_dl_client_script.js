83982bbaf5f3ae6532880f10239d7fb05d64c0cd
mfg637 <mfg636@gmail.com>
2021-07-18 21:00:56 +0300

    userscript update: image view page support

--- derpibooru_dl_client_script.js
+++ derpibooru_dl_client_script.js
@@ -2,21 +2,17 @@
 // @name        derpibooru_dl_client_script
 // @namespace   Violentmonkey Scripts
 // @match       https://derpibooru.org/
-// @match       https://derpibooru.org/images
+// @match       https://derpibooru.org/images*
 // @match       https://derpibooru.org/search
 // @match       https://derpibooru.org/tags/*
-// @match       https://twibooru.org/
-// @match       https://twibooru.org/posts
-// @match       https://twibooru.org/search
-// @match       https://twibooru.org/search/index
-// @match       https://twibooru.org/tags/*
+// @match       https://twibooru.org/*
 // @match       https://ponybooru.org/
-// @match       https://ponybooru.org/images
+// @match       https://ponybooru.org/images*
 // @match       https://ponybooru.org/search
 // @match       https://ponybooru.org/tags/*
 // @connect     localhost:5757
 // @grant       GM.xmlHttpRequest
-// @version     1.1.1
+// @version     1.1.2
 // @author      mfg637
 // @description 12.03.2021, 13:25:40
 // ==/UserScript==
@@ -49,18 +45,8 @@ function dl_button_click_handler(event){
   return false;
 }
 
-function image_handler(image_wrapper){
-  if (!(image_wrapper instanceof Element)){return;}
-  data_wrapper = image_wrapper.getElementsByClassName('image-container')[0];
-  let raw_data = data_wrapper.dataset, data = {};
-  for (let key in raw_data){data[key]=raw_data[key];}
-  data.representations=JSON.parse(data.uris);
-  delete data.uris;
-  let dl_button = document.createElement('a');
+function button_placer_default(dl_button, image_wrapper){
   dl_button.innerText='dl';
-  dl_button.data = data;
-  dl_button.onclick = dl_button_click_handler;
-  dl_button.style.fontWeight = 'Bold';
   if (document.domain === "derpibooru.org"){
     dl_button.href = url_head;
     image_wrapper.getElementsByClassName('media-box__header')[0].appendChild(dl_button);
@@ -73,5 +59,41 @@ function image_handler(image_wrapper){
   }
 }
 
+function button_placer_show_image(dl_button, unused_arg){
+  dl_button.innerText='DDL';
+  if (document.domain === "derpibooru.org"){
+    dl_button.href = url_head;
+  }else if (document.domain === 'twibooru.org'){
+    dl_button.href = url_head + "/twibooru";
+  }else if (document.domain === "ponybooru.org"){
+    dl_button.href = url_head + "/ponybooru";
+  }
+  document.getElementsByClassName('image-metabar')[0].appendChild(dl_button);
+}
+
+function image_handler(data_wrapper, button_placer, bp_arg=null){
+  let raw_data = data_wrapper.dataset, data = {};
+  for (let key in raw_data){data[key]=raw_data[key];}
+  data.representations=JSON.parse(data.uris);
+  delete data.uris;
+  let dl_button = document.createElement('a');
+  dl_button.data = data;
+  dl_button.onclick = dl_button_click_handler;
+  dl_button.style.fontWeight = 'Bold';
+  button_placer(dl_button, bp_arg);
+}
+
 image_wrappers = document.getElementsByClassName('media-box');
-for (let i in image_wrappers){image_handler(image_wrappers[i]);}
+if (image_wrappers.length>0)
+  for (let i in image_wrappers){
+    if (!(image_wrappers[i] instanceof Element)){continue}
+    data_wrapper = image_wrappers[i].getElementsByClassName('image-container')[0];
+    image_handler(data_wrapper, button_placer_default, image_wrappers[i]);
+  }
+else {
+  image_wrappers = document.getElementsByClassName('image-show-container');
+  if (image_wrappers.length>0){
+    data_wrapper = image_wrappers[0];
+    image_handler(data_wrapper, button_placer_show_image);
+  }
+}
