fbc25767ef68891e70b39e3d096f2a07d75d723e
mfg637 <mfg636@gmail.com>
2022-07-25 16:28:04 +0300

    remove redundant code

--- browser/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import net, images, gui
-
-def pil_unlook_load_truncated_images():
-    import PIL.ImageFile
-    PIL.ImageFile.LOAD_TRUNCATED_IMAGES = True
-
-pil_unlook_load_truncated_images()
