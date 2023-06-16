9861f125a44a62325c0b7a691f1c20c71a8f923a
mfg637 <mfg636@gmail.com>
2023-06-16 15:13:27 +0300

    copy project init script from MfGMediaServer project

---
+++ init-create-react-babel-project.sh
@@ -0,0 +1,12 @@
+# Initiate a project
+yarn init -y
+
+# Install React
+yarn add react react-dom
+
+# Install Webpack and loaders
+yarn add -D webpack webpack-cli webpack-dev-server
+yarn add -D @babel/core @babel/preset-env @babel/preset-react babel-loader file-loader
+yarn add -D style-loader css-loader
+yarn add -D sass-loader sass
+yarn add -D mini-css-extract-plugin
