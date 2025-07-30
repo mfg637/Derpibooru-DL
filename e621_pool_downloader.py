39762b42a2887575a221a8adb5d0a4b424755cf3
mfg637 <mfg636@gmail.com>
2025-07-30 17:17:40 +0300

    refactor: improve code formatting and structure in e621_pool_downloader.py

--- e621_pool_downloader.py
+++ e621_pool_downloader.py
@@ -9,8 +9,6 @@ import json
 import logging
 import argparse
 
-import math
-
 import download_manager
 import medialib_db
 import pyimglib
@@ -23,12 +21,16 @@ except ImportError as e:
     print('error importing config.py')
     raise e
 
-logging.basicConfig(level=logging.INFO, format="%(process)dx%(thread)d::%(levelname)s::%(name)s::%(message)s")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(process)dx%(thread)d::%(levelname)s::%(name)s::%(message)s"
+)
 
 logger = logging.getLogger(__name__)
 
 error_str1 = 'Error: user API key required.'
-error_str1_continue = 'Find you API key in page: "https://derpibooru.org/pages/api"'
+error_str1_continue = \
+    'Find you API key in page: "https://derpibooru.org/pages/api"'
 
 tasks = []
 
@@ -48,7 +50,9 @@ current_page = 1
 
 argument_parser = argparse.ArgumentParser()
 argument_parser.add_argument("pool_id", type=int)
-argument_parser.add_argument("--rewrite", help="force to rewrite existing files", action="store_true")
+argument_parser.add_argument(
+    "--rewrite", help="force to rewrite existing files", action="store_true"
+)
 args = argument_parser.parse_args()
 
 pool_id = vars(args)['pool_id']
@@ -63,11 +67,16 @@ album_id = None
 album_order_dict: dict[int, int] = dict()
 
 try:
-    request_url = ("https://e621.net/pools/{}.json?login={}&api_key={}").format(
+    request_url = (
+        "https://e621.net/pools/{}.json?login={}&api_key={}"
+    ).format(
         pool_id, config.e621_login, config.e621_API_KEY
     )
     headers = {
-        'User-Agent': 'e621_pool_downloader.py (by mfg637) (https://github.com/mfg637/Derpibooru-DL)'
+        'User-Agent': (
+            'e621_pool_downloader.py (by mfg637) '
+            '(https://github.com/mfg637/Derpibooru-DL)'
+        )
     }
     print("request url:", request_url)
     request_data = requests.get(request_url, headers=headers)
@@ -83,7 +92,6 @@ try:
 
     pool_title: str = pool_data['name'].replace("_", " ")
     print("Pool title is {}".format(pool_title))
-
 
     for album_order, origin_content_id in enumerate(pool_data['post_ids']):
         _parser = parser.e621.E621Parser(str(origin_content_id))
@@ -102,21 +110,35 @@ try:
         if album_artist is None:
             album_artist = parsed_tags["artist"].copy().pop()
 
-        set_id = medialib_db.tags_indexer.check_tag_exists(pool_title, 'set', medialib_db_connection)
+        set_id = medialib_db.tags_indexer.check_tag_exists(
+            pool_title, 'set', medialib_db_connection
+        )
         if set_id is None:
             set_id = medialib_db.tags_indexer.insert_new_tag(
-                pool_title, 'set', "set: {}".format(pool_title), medialib_db_connection
+                pool_title,
+                'set',
+                "set: {}".format(pool_title),
+                medialib_db_connection
             )
 
-        artist_id = medialib_db.tags_indexer.check_tag_exists(album_artist, 'artist', medialib_db_connection)
+        artist_id = medialib_db.tags_indexer.check_tag_exists(
+            album_artist, 'artist', medialib_db_connection
+        )
         if artist_id is None:
             artist_id = medialib_db.tags_indexer.insert_new_tag(
-                album_artist, 'artist', "artist:{}".format(pool_title), medialib_db_connection
+                album_artist,
+                'artist',
+                "artist:{}".format(pool_title),
+                medialib_db_connection
             )
 
-        album_id = medialib_db.get_album_id(set_id, artist_id, medialib_db_connection)
+        album_id = medialib_db.album.get_album_id(
+            set_id, artist_id, medialib_db_connection
+        )
         if album_id is None:
-            album_id = medialib_db.make_album(set_id, artist_id, medialib_db_connection)
+            album_id = medialib_db.album.make_album(
+                set_id, artist_id, medialib_db_connection
+            )
 
         content_info = medialib_db.find_content_from_source(
             _parser.get_origin_name(), _parser.getID(), medialib_db_connection
@@ -124,7 +146,12 @@ try:
 
         if content_info is not None:
             content_id = content_info[0]
-            medialib_db.set_album_order(album_id, content_id, album_order, medialib_db_connection)
+            medialib_db.add_tags_for_content_by_tag_ids(
+                content_id, [set_id,], medialib_db_connection
+            )
+            medialib_db.album.set_album_order(
+                album_id, content_id, album_order, medialib_db_connection
+            )
             medialib_db_connection.commit()
             continue
         else:
@@ -155,7 +182,12 @@ for origin_content_id in album_order_dict:
     )
 
     content_id = content_info[0]
-    medialib_db.set_album_order(album_id, content_id, album_order_dict[origin_content_id], medialib_db_connection)
+    medialib_db.album.set_album_order(
+        album_id,
+        content_id,
+        album_order_dict[origin_content_id],
+        medialib_db_connection
+    )
 
     medialib_db_connection.commit()
 
