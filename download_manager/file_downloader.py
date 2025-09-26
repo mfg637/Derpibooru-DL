7246bc5788c9c396faa32c2f0b8c5cf2c87c3d0c
mfg637 <mfg636@gmail.com>
2025-09-26 21:02:43 +0300

    remove transcoding functionality and medialib dependency

--- download_manager/file_downloader.py
+++ download_manager/file_downloader.py
@@ -1,8 +1,6 @@
 import logging
 import os
 
-import PIL.Image
-
 import config
 from .download_manager import DownloadManager
 import parser
@@ -14,9 +12,16 @@ class FileDownloader(DownloadManager):
     def __init__(self, _parser: parser.Parser):
         super().__init__(_parser)
 
-    def _download_body(self, src_url, name, src_filename, output_directory: str, data: dict, tags):
+    def _download_body(
+        self,
+        src_url,
+        name,
+        src_filename,
+        output_directory: str,
+        data: dict,
+        tags,
+    ):
         if self.is_rewriting_allowed() or not os.path.isfile(src_filename):
             if not config.simulate:
                 self.download_file(src_filename, src_url)
                 return 0, 0, 0, 0, src_filename
-
